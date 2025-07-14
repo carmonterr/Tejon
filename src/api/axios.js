19fd14f712c47d5da181118f90bac9ed9d054136
carmonterr <jcmonterroza7@misena.edu.co>
2025-07-14 12:52:22 -0500

    actualizo axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true,
+  // solo se usa con cookies withCredentials: true,
 })
 
 API.interceptors.request.use((config) => {
